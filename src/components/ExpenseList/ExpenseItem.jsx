5365736f4a12fdbbed4993dd20b84618e1a88fb4
Jose Ignacio <ji.ccdev@outlook.com>
2022-12-19 23:57:30 -0300

    feat: basic switeable

--- src/components/ExpenseList/ExpenseItem.jsx
+++ src/components/ExpenseList/ExpenseItem.jsx
@@ -7,6 +7,15 @@ import IconFood from '../../img/icons/icono_comida.svg';
 import IconHealth from '../../img/icons/icono_salud.svg';
 import IconSubscriptions from '../../img/icons/icono_suscripciones.svg';
 
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
+
 const ExpenseItem = ({ expense, expenseList, setExpenseList }) => {
   const { id, name, amount, category, date } = expense;
 
@@ -18,22 +27,37 @@ const ExpenseItem = ({ expense, expenseList, setExpenseList }) => {
     subscriptions: IconSubscriptions,
   };
 
+  const leadingActions = () => {
+    console.log('edit');
+  };
+
+  const trailingActions = () => {
+    console.log('delete');
+  };
+
   return (
-    <li className="gasto sombra">
-      <div className="contenido-gasto">
-        <img src={iconDictionary[category]} alt={`icon-${category}`} />
-        <div className="descripcion-gasto">
-          <p className="categoria">{category}</p>
-          <p className="nombre-gasto">{name}</p>
-          <p className="monto">
-            Created at: <span>{parseDate(date)}</span>
-          </p>
-        </div>
-      </div>
-      <div>
-        <p className="cantidad-gasto">{parseToDolar(amount)}</p>
-      </div>
-    </li>
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions}
+        trailingActions={trailingActions}
+      >
+        <li className="gasto sombra">
+          <div className="contenido-gasto">
+            <img src={iconDictionary[category]} alt={`icon-${category}`} />
+            <div className="descripcion-gasto">
+              <p className="categoria">{category}</p>
+              <p className="nombre-gasto">{name}</p>
+              <p className="monto">
+                Created at: <span>{parseDate(date)}</span>
+              </p>
+            </div>
+          </div>
+          <div>
+            <p className="cantidad-gasto">{parseToDolar(amount)}</p>
+          </div>
+        </li>
+      </SwipeableListItem>
+    </SwipeableList>
   );
 };
 
